d2f0c19492ff63153e2c31eec1f4bd310bc2a526
infoprints <160508284+infoprints@users.noreply.github.com>
2024-02-19 17:07:19 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,36 @@
-$email = $_POST["email"]; // Make sure the form field name matches
-$name = $_POST["name"];
-$linkedinLink = $_POST["message"]; // Assuming "message" is the LinkedIn link
-$to = "facelessintech@gmail.com";
-$subject = "New Request for LinkedIn Banner";
-$headers = "From: $email\n";
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// Include PHPMailer Autoloader
+require 'path/to/PHPMailer/src/PHPMailer.php';
+require 'path/to/PHPMailer/src/SMTP.php';
+require 'path/to/PHPMailer/src/Exception.php';
+
+// Create a new PHPMailer instance
+$mail = new PHPMailer(true);
+
+// Set mailer to use SMTP
+
+
+// Set sender email and name
+$mail->setFrom($email, $name);
+
+// Set recipient email
+$mail->addAddress('facelessintech@gmail.com');
+
+// Set email subject
+$mail->Subject = 'New Request for LinkedIn Banner';
 
 // Construct the message
-$message = "Name: $name\n";
-$message .= "Email: $email\n";
-$message .= "LinkedIn Link: $linkedinLink\n";
+$mail->Body = "Name: $name\nEmail: $email\nLinkedIn Link: $linkedinLink";
 
-mail($to, $subject, $message, $headers);
+// Send the email
+try {
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
