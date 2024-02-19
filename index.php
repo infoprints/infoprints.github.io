6e5cd2b5d9dfe3a5882a8ef6cc22c5f215b655a5
infoprints <160508284+infoprints@users.noreply.github.com>
2024-02-19 16:54:46 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
-<?PHP
-	$email = $_POST["emailaddress"];
-	$to = "facelessintech@gmail.com";
-	$subject = "New Request for LinkedIn Banner";
-	$headers = "From: $email\n";
-	$message = " $message ";
-	
-	mail($to,$subject,$message,$headers);
-?>
+$email = $_POST["email"]; // Make sure the form field name matches
+$name = $_POST["name"];
+$linkedinLink = $_POST["message"]; // Assuming "message" is the LinkedIn link
+$to = "facelessintech@gmail.com";
+$subject = "New Request for LinkedIn Banner";
+$headers = "From: $email\n";
+
+// Construct the message
+$message = "Name: $name\n";
+$message .= "Email: $email\n";
+$message .= "LinkedIn Link: $linkedinLink\n";
+
+mail($to, $subject, $message, $headers);
